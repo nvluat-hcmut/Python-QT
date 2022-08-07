87cda0ac13561d4c6411145b40387756568356e0
nvluat-hcmut <nguyenvanluat302@gmail.com>
2022-08-07 23:22:58 +0700

    QPushButton and QLabel

---
+++ main.py
@@ -0,0 +1,34 @@
+from ctypes import alignment
+from pickletools import pyset
+import PySide6.QtCore
+
+print(PySide6.__version__)
+print(PySide6.QtCore.__version__)
+
+import sys
+import random
+from PySide6 import QtCore,QtWidgets,QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.hello = ["Hello everyone", "Xin chao", "A nhong a xe o"]
+        self.button = QtWidgets.QPushButton("Click me")
+        self.text = QtWidgets.QLabel("Hello World", alignment = QtCore.Qt.AlignCenter)
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
